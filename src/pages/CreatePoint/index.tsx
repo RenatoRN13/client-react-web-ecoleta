0594a5677798326aa7872d42ac68fcc31787e31e
Renato Filho <renatocrvg@info.ufrn.br>
2020-06-30 00:28:57 -0300

    Initial Commit
    Partial development of the system Ecoleta

---
+++ src/pages/CreatePoint/index.tsx
@@ -0,0 +1,176 @@
+import React, { useEffect, useState, ChangeEvent } from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+
+import api from '../../services/api';
+import axios from 'axios';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
+
+interface Items {
+    id: number;
+    title: string;
+    image_url: string;
+}
+
+interface IBGEUFResponse {
+    sigla: string;
+}
+
+interface IBGECityResponse {
+    nome: string;
+}
+
+const CreatePoint = () => {
+    const [items, setItems] = useState<Items[]>([]);
+    const [ufs, setUfs] = useState<string[]>([]);
+    const [cities, setCities] = useState<string[]>([]);
+
+    const [selectedUf, setSelectedUf] = useState('0');
+
+
+    useEffect(() => { 
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
+
+    useEffect(() => { 
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados/').then(response => {
+            const ufInitials = response.data.map(uf => uf.sigla);
+
+            setUfs(ufInitials);
+        });
+    }, []);
+
+    useEffect(() => {
+        if(selectedUf === '0'){
+            return;
+        }
+
+        axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`).then(response => {
+            const cities = response.data.map(city => city.nome);
+
+            setCities(cities);
+        });
+
+    });
+
+    function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
+        const uf = event.target.value;
+
+        setSelectedUf(uf);
+    }
+
+    return (
+        <div id="page-create-point">
+            <header> 
+                <img src={logo} alt="Ecoleta" />
+
+                <Link to="/">
+                    <FiArrowLeft />
+                    Voltar para home
+                </Link>
+            </header>
+
+            <form>
+                <h1>Cadastro do ponto de coleta</h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+
+                    <div className="field">
+                        <label htmlFor="name">Nome da entidade</label>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name" />
+                    </div>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email">E-mail</label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email" />
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="whatsapp">Whatsapp</label>
+                            <input
+                                type="text"
+                                name="whatsapp"
+                                id="whatsapp" />
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione o endereço no mapa</span>
+                    </legend>
+
+                    <Map center={[-5.8850813, -35.2038551]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-5.8850813, -35.2038551]} />
+                    </Map>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select name="uf" id="uf" onChange={handleSelectUf}>
+                                <option value="0">Selecione uma UF</option>
+                                {ufs.map(uf => (
+                                    <option key={uf} value={uf}>{uf}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="cityf">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma cidade</option>
+                                {cities.map(city => (
+                                    <option key={city} value={city}>{city}</option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Itens de coleta</h2>
+                        <span>Selecione um ou mais itens abaixo</span>
+                    </legend>
+
+                    <ul className="items-grid">
+                        {items.map(item => (
+                            <li key={item.id} >
+                                <img src={item.image_url} alt={item.title} />
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </fieldset>
+
+                <button type="submit">
+                    Cadastrar ponto de coleta
+                </button>
+            </form>
+        </div>
+    )
+};
+
+
+export default CreatePoint;
